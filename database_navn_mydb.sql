2dcf685aff3e542c4ff69b41ee659212278b1b0e
ninjinv <144213393+ninjinv@users.noreply.github.com>
2023-12-07 13:37:36 +0100

    legger til database
    dbfil

---
+++ database_navn_mydb.sql
@@ -0,0 +1,48 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET latin1 ;
+USE `mydb` ;
+
+-- -----------------------------------------------------
+-- Table `mydb`.`users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`users` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `navn` VARCHAR(40) NOT NULL,
+  `username` VARCHAR(40) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `username_UNIQUE` (`username` ASC),
+  INDEX `idx_users_id` (`id` ASC))
+ENGINE = InnoDB
+AUTO_INCREMENT = 21
+DEFAULT CHARACTER SET = latin1;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`users_checkpoint`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`users_checkpoint` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `checkpoints` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`, `checkpoints`),
+  INDEX `fk_users_checkpoint_user_progress1_idx` (`checkpoints` ASC),
+  INDEX `idx_users_checkpoint` (`id` ASC))
+ENGINE = InnoDB
+AUTO_INCREMENT = 7
+DEFAULT CHARACTER SET = latin1;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
